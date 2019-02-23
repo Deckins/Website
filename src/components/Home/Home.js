e98021f01629f61b6494ff48f77a15b0a18029c7
Deckins <bennythai2013@hotmail.com>
2019-02-23 16:46:46 -0500

    Image routing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ class Home extends Component {
     render() {
         return (
             <div className='black'>
-                <Image src='../../../assets/bear.jpeg'
+                <Image src='/assets/images/bear.jpeg'
                     className='header-image' />
                 <Jumbotron className='jumbotron'>
                     <h1>Looks like you've stumbled to the right place!</h1>
