568c312b9bbbdfa4618687bfa83d5cd328ec4ef9
Deckins <bennythai2013@hotmail.com>
2019-03-06 21:42:20 -0500

    Background image centered and more img home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,13 +6,20 @@ class Home extends Component {
     render() {
         return (
             <div className='black'>
-                 <Image src='/Website/assets/images/bear.jpg'
-                    className='header-image' />
+                <div >
+                    <Image src='/assets/images/cat.jpg'
+                     className='header-image'/>
+                    <Image src='/assets/images/bear.jpg'
+                     className='header-image'/>
+                    <Image src='/assets/images/dog.jpg'
+                     className='header-image'/>
+                </div>
+                 
                 <Jumbotron className='jumbotron'>
                     <h5>
                        Feel free to browse my webpage
                     </h5>
-                    <h5>and contact me if you have any questions or comments!</h5>
+                    
                 </Jumbotron>
                 <Footer/>
             </div>
