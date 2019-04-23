489c811873a68b5004931c390d15e082c987ff8f
Deckins <bennythai2013@hotmail.com>
2019-04-23 00:39:03 -0400

    Home and project styling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,39 +1,21 @@
 import React, { Component } from 'react';
-import { Image, Jumbotron } from 'react-bootstrap';
 import './Home.css';
 import Footer from '../Footer/Footer';
 class Home extends Component {
-    state = {
-        clicked: false,
-        choice: ''
-    }
-    clickHandler = (id) => {
-        console.log(id)
-        this.setState({ clicked: true, choice: id })
-    }
+    
     render() {
-        let affiliation = <h3>Welcome to my Website</h3>;
-        if (this.state.clicked === true) {
-            if (this.state.choice === 'dog')
-                affiliation = <div>Looks like you've chosen Dog</div>
-            else if (this.state.choice === 'cat')
-                affiliation = <div>Looks like you've chosen Cat</div>
-            else if (this.state.choice === 'bear')
-                affiliation = <div>Looks like you've chosen Bear</div>
-
-
-        }
         return (
             <div>
                     <div className='img-wrapper'>
-                        <img className='bridge' src='../../assets/images/city.jpg' />
+                        <img className='bridge' src='/Website/assets/images/city.jpg' />
                     </div>
                     <div className='font-body'>
                         <blockquote>
-                            The man who comes back through the door in the wall will
-                            never be quite the same as the man who went out.
+                        Inaction breeds doubt and fear. Action breeds confidence and courage. 
+                        If you want to conquer fear, do not sit home and think about it. 
+                        Go out and get busy
                         </blockquote>
-                        <cite>Aldous Huxley</cite>
+                        <cite>Dale Carnegie</cite>
                     </div>
                 <Footer />
             </div>
